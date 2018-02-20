8028e1b6e460d71f2bf7342ab17187edb502be13
Aleksandar Batin <aleksandarbatin@gmail.com>
2018-02-20 16:05:41 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+// /////////////////////////////////////////////////
+// GULP
+// /////////////////////////////////////////////////
+
+var gulp = require('gulp');
+
+// /////////////////////////////////////////////////
+// GULP tasks
+// /////////////////////////////////////////////////
+
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var uglify = require('gulp-uglify');
+var minifyCSS = require('gulp-minify-css');
+var imagemin = require('gulp-imagemin');
+var browserSync = require("browser-sync");
+var reload = browserSync.reload;
+
+// /////////////////////////////////////////////////
+// COMPRESS IMAGES
+// /////////////////////////////////////////////////
+
+gulp.task('compress-images', function(){
+	return gulp.src('src/pre-images/**/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('dist/images/'));
+});
+
+// /////////////////////////////////////////////////
+// STYLES
+// /////////////////////////////////////////////////
+
+gulp.task('styles', function(){
+  return gulp.src('src/sass/**/*.scss')
+  	.pipe(plumber())
+    .pipe(sass()) // Using gulp-sass
+    .pipe(gulp.dest('src/pre-css'))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(reload({stream:true}));
+});
+
+
+// /////////////////////////////////////////////////
+// HTML Task
+// /////////////////////////////////////////////////
+
+gulp.task("html", function(){
+    gulp.src("dist/**/*.html")
+    .pipe(reload({stream:true}));
+});
+
+// /////////////////////////////////////////////////
+// Browser-Sync Task
+// /////////////////////////////////////////////////
+
+gulp.task('browser-sync', function(){
+  browserSync({
+    server:{
+      baseDir:"./dist/"
+    }
+  })
+});
+
+// /////////////////////////////////////////////////
+// WATCH
+// /////////////////////////////////////////////////
+gulp.task('watch', function(){
+  gulp.watch('src/sass/**/*.scss', ['styles']); 
+});
+
+// /////////////////////////////////////////////////
+// UGLIFY MAIN.JS
+// /////////////////////////////////////////////////
+
+gulp.task('uglify', function(){
+	gulp.src('src/js/*')
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js/'));
+});
+
+// /////////////////////////////////////////////////
+// Default Task
+// /////////////////////////////////////////////////
+
+gulp.task('deploy',[ 'uglify', 'compress-images' ]);
+
+
+// /////////////////////////////////////////////////
+// Default Task
+// /////////////////////////////////////////////////
+
+gulp.task('default',[ 'styles', 'uglify', 'html', 'browser-sync', 'watch' ]);
